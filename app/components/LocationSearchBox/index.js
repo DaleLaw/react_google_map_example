16e8bd808a5c2cc996251bc696292fe0f07c73fd
Dale Law <developerdalelaw@gmail.com>
2017-09-23 11:42:21 +0800

    Add remove button in LocationSearchBox

--- app/components/LocationSearchBox/index.js
+++ app/components/LocationSearchBox/index.js
@@ -3,21 +3,43 @@ import styled from 'styled-components';
 import PropTypes from 'prop-types';
 import Geosuggest from 'react-geosuggest';
 
-export const StyledGeosuggest = styled(Geosuggest)`
+export const Wrapper = styled.div`
+  background: white;
+  boxSizing: border-box;
+  border: 1px solid transparent;
+  width: 240px;
+  height: 44px;
+  margin: 8px auto 8px auto;
+  borderRadius: 3px;
+  boxShadow: 0 2px 6px rgba(0, 0, 0, 0.3);
+`;
+
+export const RemoveButton = styled.button`
+  display: inline-block;
+  color: grey;
+  width: 44px;
+  height: 44px;
+  outline: none;
+  cursor: pointer;
+  &:hover {
+    color: #008CBA
+    transition-duration: 0.4s;
+  }
+`;
+
+const StyledGeosuggest = styled(Geosuggest)`
+  display: inline-block;
+  width: calc(100% - 44px);
+
   & input {
-    fontSize: 14px;
-    display: block;
-    background: white;
-    boxSizing: border-box;
-    border: 1px solid transparent;
-    width: 240px;
+    vertical-align: center;
+    width: 100%;
     height: 44px;
-    margin: 8px auto 8px auto;
+    line-height: 44px; 
     padding: 0 12px;
-    borderRadius: 3px;
-    boxShadow: 0 2px 6px rgba(0, 0, 0, 0.3);
-    outline: none;
+    fontSize: 14px;
     textOverflow: ellipses;
+    outline: none;
   }
 
   & .geosuggest__suggests--hidden {
@@ -31,9 +53,8 @@ export const StyledGeosuggest = styled(Geosuggest)`
     background: white;
     color: grey;
     list-style: none;
-    width: 244px;
+    width: 240px;
     margin-top: 0px;
-    margin-left: 28px;
     padding: 0px;
     z-index: 1;
     boxShadow: 0 2px 6px rgba(0, 0, 0, 0.3);
@@ -56,18 +77,27 @@ class LocationSearchBox extends React.PureComponent { // eslint-disable-line rea
     onSuggestSelect: PropTypes.func.isRequired,
     placeholder: PropTypes.string.isRequired,
     location: PropTypes.object.isRequired,
+    onClickRemove: PropTypes.func.isRequired,
   }
 
   render() {
-    const { onSuggestSelect, placeholder, location } = this.props;
+    const { onSuggestSelect, placeholder, location, onClickRemove } = this.props;
     return (
-      <StyledGeosuggest
-        type={'text'}
-        placeholder={placeholder}
-        onSuggestSelect={onSuggestSelect}
-        location={location}
-        radius="20"
-      />
+      <Wrapper>
+        <StyledGeosuggest
+          type={'text'}
+          placeholder={placeholder}
+          onSuggestSelect={onSuggestSelect}
+          location={location}
+          radius="20"
+        />
+        <RemoveButton
+          title={'Remove Location'}
+          onClick={() => onClickRemove()}
+        >
+          X
+        </RemoveButton>
+      </Wrapper>
     );
   }
 }
--- app/components/LocationSearchBox/tests/index.test.js
+++ app/components/LocationSearchBox/tests/index.test.js
@@ -1,16 +1,28 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import LocationSearchBox, { StyledGeosuggest } from '../index';
+import LocationSearchBox, { Wrapper, RemoveButton } from '../index';
 
 describe('<LocationSearchBox />', () => {
-  it('should render StyledGeosuggest', () => {
-    const generatedComponent = shallow(
-      <LocationSearchBox
-        onSuggestSelect={jest.fn()}
-        placeholder={'Test Placeholder'}
-        location={{ lat: jest.fn(() => 10), lng: jest.fn(() => 20) }}
-      />
-    );
-    expect(generatedComponent.type()).toEqual(StyledGeosuggest);
+  const generateComponent = (props = {}) => shallow(
+    <LocationSearchBox
+      onSuggestSelect={jest.fn()}
+      placeholder={'Test Placeholder'}
+      location={{ lat: jest.fn(() => 10), lng: jest.fn(() => 20) }}
+      onClickRemove={jest.fn()}
+      {...props}
+    />
+  );
+  it('should render Wrapper', () => {
+    const generatedComponent = generateComponent();
+    expect(generatedComponent.type()).toEqual(Wrapper);
+  });
+
+  it('should call onClickRemove when remove button is clicked', () => {
+    const mockCallback = jest.fn();
+    const generatedComponent = generateComponent({
+      onClickRemove: mockCallback,
+    });
+    generatedComponent.find(RemoveButton).simulate('click');
+    expect(mockCallback).toHaveBeenCalledTimes(1);
   });
 });
